fc5bbbbada49b30d2140dad54c06d8708e253e99
NoodleSushi <ytmikesstuffs@gmail.com>
2023-03-10 16:20:51 +0800

    Created TicTacToeGame and temporary UI

--- Tic-Tac-Toe/Tic-Tac-Toe/MainPage.xaml.cs
+++ Tic-Tac-Toe/Tic-Tac-Toe/MainPage.xaml.cs
@@ -10,9 +10,45 @@ namespace Tic_Tac_Toe
 {
     public partial class MainPage : ContentPage
     {
+        private TicTacToeGame Game = new TicTacToeGame();
+        private Button[] BoxList;
+
         public MainPage()
         {
             InitializeComponent();
+            BoxList = new Button[] {Box0, Box1, Box2, Box3, Box4, Box5, Box6, Box7, Box8};
+            Game.BoxChangeHandler = new PlayerXHandler(BoxChangeNotified);
+            Game.PlayerTurnHandler = new PlayerHandler(PlayerTurnNotified);
+            Game.WinnerHandler = new PlayerHandler(WinnerNotified);
+            Game.Reset();
+        }
+
+        private void OnBoxClicked(object sender, EventArgs e)
+        {
+            Game.PlacePiece(int.Parse((sender as Button).ClassId));
+        }
+
+        private void ResetGame(object sender, EventArgs e)
+        {
+            Game.Reset();
+        }
+
+        void BoxChangeNotified(int x, int player)
+        {
+            BoxList[x].Text = (player == -1) ? "-" : (player == 0) ? "X" : "O"; 
+            BoxList[x].BackgroundColor = (player == -1) ? Color.Gray : (player == 0) ? Color.Red : Color.Blue;
+        }
+
+        void PlayerTurnNotified(int player)
+        {
+            XStatus.Text = "X " + ((player == 0) ? "player's turn" : "wait");
+            OStatus.Text = "O " + ((player == 1) ? "player's turn" : "wait");
+        }
+
+        void WinnerNotified(int player)
+        {
+            XStatus.Text = "X player " + ((player == 0) ? "won" : "lost");
+            OStatus.Text = "O player " + ((player == 1) ? "won" : "lost");
         }
     }
 }
---
+++ Tic-Tac-Toe/Tic-Tac-Toe/TicTacToeGame.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tic_Tac_Toe
+{
+    public delegate void PlayerHandler(int player);
+    public delegate void PlayerXHandler(int x, int player);
+
+    public class TicTacToeGame
+    {
+        // int player:
+        // -1 - None
+        // 0 - X
+        // 1 - O
+        public PlayerXHandler BoxChangeHandler;
+        public PlayerHandler PlayerTurnHandler;
+        public PlayerHandler WinnerHandler;
+        private int BoardPieceMask = 0;
+        private int BoardPlayerMask = 0;
+        private int Turn = 0;
+        private int Winner = -1;
+        private int[] WinningStates = {
+            0b000000111,
+            0b000111000,
+            0b111000000,
+            0b001001001,
+            0b010010010,
+            0b100100100,
+            0b100010001,
+            0b001010100,
+        };
+
+        public void Reset()
+        {
+            BoardPieceMask = 0;
+            BoardPlayerMask = 0;
+            Turn = 0;
+            Winner = -1;
+            for (int x = 0; x < 9; x++)
+                BoxChangeHandler.Invoke(x, -1);
+            PlayerTurnHandler.Invoke(Turn);
+        }
+
+        public void PlacePiece(int x)
+        {
+            if (((BoardPieceMask >> x) & 1) == 1 || Winner != -1)
+                return;
+            BoardPieceMask |= 1 << x;
+            BoardPlayerMask |= Turn << x;
+            BoxChangeHandler.Invoke(x, Turn);
+            Winner = GetWinner();
+            if (Winner != -1)
+                WinnerHandler.Invoke(Winner);
+            else
+            {
+                Turn ^= 1;
+                PlayerTurnHandler.Invoke(Turn);
+            }
+        }
+
+        public int GetWinner()
+        {
+            foreach (int winState in WinningStates)
+            {
+                bool arePiecesLinedUp = (BoardPieceMask & winState) == winState;
+                int playerCheck = BoardPlayerMask & winState;
+                bool hasPlayerWon = playerCheck == 0 || playerCheck == winState;
+                if (arePiecesLinedUp && hasPlayerWon)
+                    return (playerCheck == 0) ? 0 : 1;
+            }
+            return -1;
+        }
+    }
+}
